b6bb89319e94d5018ff975e7148a00592f76ddf1
Darpan <darpanyadav437@gmail.com>
2026-06-14 23:37:10 +0530

    New Ui

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router  = express.Router();
 const protect = require("../middleware/authMiddleware");
-const { upload, getServices, createService, deleteService, addWork, deleteWork } =
+const { upload, getServices, createService, deleteService, addWork, deleteWork, updateThumbnail } =
   require("../controllers/serviceController");
 
 router.get("/",                      getServices);
 router.post("/",                     protect, upload.single("media"), createService);
 router.delete("/:id",                protect, deleteService);
+router.patch("/:id/thumbnail",       protect, upload.single("media"), updateThumbnail);
 router.post("/:id/works",            protect, upload.single("media"), addWork);
 router.delete("/:id/works/:workId",  protect, deleteWork);
 
